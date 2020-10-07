a4799edb50a002b03a54f45240b7e8d8ad2d685a
Tom Osika <tommycollege558@gmail.com>
2020-10-07 12:07:04 -0400

    Initial django files

---
+++ parflow_data_management/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from parflow_data_management.users.models import User
+from parflow_data_management.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
