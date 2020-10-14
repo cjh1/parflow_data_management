44b5af9565b3f7a4440a506bd5dae05d1c42d797
Tom Osika <tommycollege558@gmail.com>
2020-10-14 08:53:08 -0400

    Add id and owner field to project. Make timestamped

--- parflow_data_management/scheduler/models/project.py
+++ parflow_data_management/scheduler/models/project.py
@@ -1,4 +1,7 @@
+from config.settings.base import AUTH_USER_MODEL
+from django_extensions.db.models import TimeStampedModel
 from django.db import models
 
-class Project(models.Model):
-    pass
+class Project(TimeStampedModel, models.Model):
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # TODO: files,
--- parflow_data_management/scheduler/rest/project.py
+++ parflow_data_management/scheduler/rest/project.py
@@ -4,4 +4,5 @@ from parflow_data_management.scheduler.models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        # TODO: Add some fields
+        fields = ("id, owner")
+
