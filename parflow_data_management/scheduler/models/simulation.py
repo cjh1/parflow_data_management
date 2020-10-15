aa923545595a7b4f5342dbff7fd8705c95ede194
Tom Osika <tommycollege558@gmail.com>
2020-10-15 15:48:50 -0400

    Remove extra imports

--- parflow_data_management/scheduler/models/simulation.py
+++ parflow_data_management/scheduler/models/simulation.py
@@ -1,8 +1,5 @@
-from config.settings.base import AUTH_USER_MODEL
 from django_extensions.db.models import TimeStampedModel
-from django.db import models
 
-from .project import Project
 from .project_asset import ProjectAsset
 
 class Simulation(TimeStampedModel, ProjectAsset):
