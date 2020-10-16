0421f759dd9b727f5702802a678464ab24d48b7f
Tom Osika <tommycollege558@gmail.com>
2020-10-16 14:07:20 -0400

    use get_user_model

--- parflow_data_management/scheduler/models/project.py
+++ parflow_data_management/scheduler/models/project.py
@@ -1,8 +1,8 @@
-from config.settings.base import AUTH_USER_MODEL
+from django.contrib.auth import get_user_model
 from django_extensions.db.models import TimeStampedModel
 from django.db import models
 
 
 class Project(TimeStampedModel, models.Model):
-    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="projects")
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="projects")
     # TODO: more fields,
