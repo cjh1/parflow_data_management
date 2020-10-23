6afa9a3629df666d072215caae2658e4a9e2b113
Tom Osika <tommycollege558@gmail.com>
2020-10-23 09:00:26 -0400

    Add default database host and port

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,11 +8,6 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
-# DATABASES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES["default"]["HOST"] = "localhost"
-
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
