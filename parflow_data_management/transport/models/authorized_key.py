e13e55bbd30d2914adebe1ac040ccdea13459d5c
Tom Osika <tommycollege558@gmail.com>
2020-11-20 15:34:52 -0500

    Replace public key with keypair in authorized keys

--- parflow_data_management/transport/models/authorized_key.py
+++ parflow_data_management/transport/models/authorized_key.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import CharField
 from django.utils.translation import gettext_lazy as _
 
 from parflow_data_management.scheduler.models.cluster import Cluster
+from parflow_data_management.transport.models.key_pair import KeyPair
 
 
-# A cluster and a valid public key for a user.
+# A cluster and a valid public key for a user. Access user through key_pair.
 class AuthorizedKey(models.Model):
     cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE, related_name="authorized_keys")
-    public_key = CharField(_("Public key for cluster"), max_length=4096)
+    key_pair = models.ForeignKey(KeyPair, on_delete=models.CASCADE, related_name="authorized_keys")
